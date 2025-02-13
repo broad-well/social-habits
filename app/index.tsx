3e5f0ca9348893af09ab6bdd6788a503ee6b0f11
Houdong LIANG <houdongliang84@gmail.com>
2025-02-13 00:27:48 -0800

    [feat] welcome, sign in, sign up pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Image, Text, View, StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
 import {
   MD3LightTheme as DefaultTheme,
@@ -10,14 +10,18 @@ import { useFonts } from "expo-font";
 import DarkThemeColors from "../constants/DarkThemeColors.json";
 import LightThemeColors from "../constants/LightThemeColors.json";
 import { useColorTheme } from "../stores/useColorTheme";
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { Link, router, Stack } from "expo-router";
 
 export default function Index() {
+  const screenOptions = {
+    headerShown: false,
+  };
+
   const [loaded] = useFonts({
     Poppins: require("../assets/fonts/Poppins/Poppins-Regular.ttf"),
+    PoppinsBold: require("../assets/fonts/Poppins/Poppins-Bold.ttf"),
   });
+  const { colorTheme } = useColorTheme();
 
   useEffect(() => {
     if (loaded) {
@@ -28,7 +32,6 @@ export default function Index() {
   if (!loaded) {
     return null;
   }
-  const { colorTheme, toggleTheme } = useColorTheme();
 
   const theme = {
     ...DefaultTheme,
@@ -38,19 +41,82 @@ export default function Index() {
 
   return (
     <PaperProvider theme={theme}>
+      <Stack.Screen options={screenOptions} />
       <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: theme.colors.primary,
-        }}
+        style={[styles.container, { backgroundColor: theme.colors.primary }]}
       >
-        <Text>Use the button below to toggle the theme:</Text>
-        <Button icon="camera" mode="contained" onPress={toggleTheme}>
-          Toggle Theme
+        <Image
+          source={require("../assets/images/icon.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <Text style={[styles.title, { color: theme.colors.onPrimary }]}>
+          Welcome to Cohabit!
+        </Text>
+        <Button
+          icon="login"
+          mode="contained"
+          onPress={() => router.push("/account/sign-in")}
+          style={[styles.button, { backgroundColor: theme.colors.onPrimary }]}
+          labelStyle={styles.buttonLabel}
+        >
+          Sign In
         </Button>
+        <View style={styles.signupContainer}>
+          <Text style={{ color: theme.colors.onPrimary }}>
+            Don't have an account?{" "}
+            <Link
+              href="/account/sign-up"
+              style={[styles.signupLink, { color: theme.colors.onPrimary }]}
+            >
+              Sign Up!
+            </Link>
+          </Text>
+        </View>
       </View>
     </PaperProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 30,
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: "PoppinsBold",
+    textAlign: "center",
+    marginBottom: 40,
+  },
+  button: {
+    width: "80%",
+    paddingVertical: 10,
+    borderRadius: 8,
+    elevation: 3,
+    marginBottom: 20,
+  },
+  buttonLabel: {
+    fontSize: 18,
+    fontFamily: "Poppins",
+    color: "#fff",
+  },
+  link: {
+    textDecorationLine: "none",
+    color: "#fff",
+  },
+  signupContainer: {
+    marginTop: 10,
+  },
+  signupLink: {
+    textDecorationLine: "underline",
+    fontWeight: "bold",
+  },
+});
