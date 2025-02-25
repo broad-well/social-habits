40a36cdaa1b3d1e3442fc17489a798a91f774f32
Michael Peng <michael@mcmoo.org>
2025-02-25 14:48:17 -0800

    Move secrets from firebaseConfig.ts into .env and GitHub Secrets

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,8 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// Initialize Firebase
+const firebaseConfig = JSON.parse(process.env["FIREBASE_CLIENT_OPTIONS"]!);
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
