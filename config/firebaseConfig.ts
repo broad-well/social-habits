224d1a361ecc38247f7886b1f48015c8f4097207
r800360 <rosachdeva@ucsd.edu>
2025-02-26 16:28:17 -0800

    Try to use expo constants in place of process.env for react native here

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,9 +1,10 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import 'dotenv/config';
+import Constants from 'expo-constants';
+
 
 // Initialize Firebase
-const firebaseConfig = JSON.parse(process.env["FIREBASE_CLIENT_OPTIONS"]!);
+const firebaseConfig = JSON.parse(Constants.expoConfig?.extra?.firebaseClientOptions);
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
