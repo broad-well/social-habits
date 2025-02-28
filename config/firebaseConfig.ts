269411643b34d16b94974b1c73556bc3d3de82f5
Achu Menon <achyutha11@gmail.com>
2025-02-28 12:38:46 -0800

    resetting firebase config file

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,10 +1,10 @@
-// import { initializeApp } from 'firebase/app';
-// import { getAuth } from 'firebase/auth';
-// import 'dotenv/config';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import 'dotenv/config';
 
-// // Initialize Firebase
-// console.log("FIREBASE_CLIENT_OPTIONS:", process.env.FIREBASE_CLIENT_OPTIONS);
-// const firebaseConfig = JSON.parse(process.env["FIREBASE_CLIENT_OPTIONS"]!);
+// Initialize Firebase
+console.log("FIREBASE_CLIENT_OPTIONS:", process.env.FIREBASE_CLIENT_OPTIONS);
+const firebaseConfig = JSON.parse(process.env["FIREBASE_CLIENT_OPTIONS"]!);
 
-// export const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
