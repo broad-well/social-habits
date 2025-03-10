e14b05c95a1343cc13844d19902d4e11e1d080b1
r800360 <rosachdeva@ucsd.edu>
2025-03-10 12:03:07 -0700

    Try to make changes to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,23 @@
 module.exports = {
     preset: "react-native",
     setupFilesAfterEnv: [
-        "<rootDir>/jest-setup.js"
+        "<rootDir>/jest-setup.js",
+        "@testing-library/jest-native/extend-expect"
     ],
     transformIgnorePatterns: [
-        "node_modules/(?!(@react-native|react-native|expo|expo-font|expo-constants)/)",
+        "node_modules/(?!(@react-native|react-native|expo|expo-font|expo-constants|radio-buttons-react-native|react-native-vector-icons|@babel/runtime)/)",
     ],
     testEnvironment: 'jsdom',
     moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/$1" // Adjust based on actual project structure
-      },
+        "^@/(.*)$": "<rootDir>/$1", // Adjust based on actual project structure
+        "^react-native-vector-icons/(.*)$": "<rootDir>/node_modules/react-native-vector-icons/lib/$1"
+    },
+    transform: {
+        "^.+\\.tsx?$": "babel-jest",
+        "^.+\\.jsx?$": "babel-jest",
+         "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+    }
 };
-
 // "jest": {
 //     "preset": "jest-expo",
 //     "setupFilesAfterEnv": [
