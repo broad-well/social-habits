7c2ab96a73a73ebbd357a822641952382d1735a1
r800360 <rosachdeva@ucsd.edu>
2025-03-11 20:21:11 -0700

    Removes codebreaking redundant lines in the babel configuration

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -3,16 +3,22 @@ module.exports = function (api) {
   return {
     presets: [
       "babel-preset-expo",
-      "@babel/preset-env",
-      "@babel/preset-react",
-      "@babel/preset-typescript"
+      "@babel/preset-typescript",
+    ],
+    plugins: [
+      ["@babel/plugin-transform-class-properties", { loose: true }],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
     ],
     env: {
       test: {
-        presets: [
-          ["@babel/preset-env", { targets: { node: "current" } }]
-        ]
-      }
-    }
+        presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+        plugins: [
+          ["@babel/plugin-transform-class-properties", { loose: true }],
+          ["@babel/plugin-transform-private-methods", { loose: true }],
+          ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+        ],
+      },
+    },
   };
 };
